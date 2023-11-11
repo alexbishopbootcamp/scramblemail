30d4cfa49b965b1d9281eb27371ed8316128fd67
Alex <github@alexbi.shop>
2023-11-11 23:22:15 +0900

    Added styling for landing page info cards

--- client/src/components/Landing/Info.jsx
+++ client/src/components/Landing/Info.jsx
@@ -1,8 +1,78 @@
 import React from 'react';
 
+
 const Info = () => {
   return (
-    <h1>Info</h1>
+    <>
+
+      <content className="flex flex-col w-full items-center justify-center text-center
+                          sm:flex-row">
+        
+          <div className="flex-grow
+                          flex flex-col
+                          items-start text-left justify-center w-full p-6">
+              <h1 className="text-2xl font-bold text-theme-blue-300">
+                <span className="text-theme-blue-300">Randomized email on every site</span>
+              </h1>
+              <p className="text-xl text-theme-blue-300">
+                Sign up on every site with a unique email address to prevent tracking.
+              </p>
+          </div>
+
+          <div className="flex-grow
+                          flex
+                          items-center justify-center text-center w-full p-6">
+            <img className="h-48 w-80" src="secure-identity-2.jpg"></img>
+          </div>
+      </content>
+      
+
+      <content className="flex flex-col w-full items-center justify-center text-center
+                          sm:flex-row-reverse">
+        
+          <div className="flex-grow
+                          flex flex-col
+                          items-start text-left justify-center w-full p-6">
+              <h1 className="text-2xl font-bold text-theme-blue-300">
+                <span className="text-theme-blue-300">Seamlessly works on any website</span>
+              </h1>
+              <p className="text-xl text-theme-blue-300">
+              Automatically generate a unique email address for every site you visit.
+              </p>
+          </div>
+
+          <div className="flex-grow
+                          flex
+                          items-center justify-center text-center w-full p-6">
+            <img className="h-48 w-80" src="secure-identity-2.jpg"></img>
+          </div>
+      </content>
+
+      <content className="flex flex-col w-full items-center justify-center text-center
+                          sm:flex-row">
+        
+          <div className="flex-grow
+                          flex flex-col
+                          items-start text-left justify-center w-full p-6">
+              <h1 className="text-2xl font-bold text-theme-blue-300">
+                <span className="text-theme-blue-300">Pre-emptive protection against data breaches</span>
+              </h1>
+              <p className="text-xl text-theme-blue-300">
+                Prevent common attacks including 
+                <span className="text-theme-blue-200"> credential stuffing </span>
+                and
+                <span className="text-theme-blue-200"> password spraying.</span> 
+              </p>
+          </div>
+
+          <div className="flex-grow
+                          flex
+                          items-center justify-center text-center w-full p-6">
+            <img className="h-48 w-80" src="secure-identity-2.jpg"></img>
+          </div>
+      </content>
+
+    </>
   );
 };
 
--- client/src/components/Landing/Splash.jsx
+++ client/src/components/Landing/Splash.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 
 const Splash = () => {
   return (
-    <content>
-      <div className="flex relative flex-col w-full items-center justify-center text-center">
-        <div className="m-8 p-4 w-full max-w-md lg:max-w-lg">
-          <h1 className="text-4xl text-theme-blue-300">
-            <span className="text-theme-blue-300">Protect your identity from </span>
-            <span className="text-theme-blue-200">data breaches </span>
-            <span className="text-theme-blue-300">and </span>
-            <span className="text-theme-blue-200">spam</span>
-          </h1>
-        </div>
-        <img className="rounded-full w-64" src="secure-identity-2.jpg"></img>
+    <content className="flex flex-col relative w-full items-center justify-center text-center pt-10;">
+      <div className="m-8 p-4 w-full max-w-md lg:max-w-lg">
+        <h1 className="text-4xl text-theme-blue-300">
+          <span className="text-theme-blue-300">Protect your identity from </span>
+          <span className="text-theme-blue-200">data breaches </span>
+          <span className="text-theme-blue-300">and </span>
+          <span className="text-theme-blue-200">spam</span>
+        </h1>
       </div>
+      <img className="rounded-full w-64" src="secure-identity-2.jpg"></img>
     </content>
   );
 };
