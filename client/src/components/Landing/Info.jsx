ec92901caaec7126a6afc3bf886f2006448ddc80
Alex <github@alexbi.shop>
2023-11-12 16:45:09 +0900

    Modularized landing page cards

--- client/src/components/Landing/Info.jsx
+++ client/src/components/Landing/Info.jsx
@@ -1,76 +1,66 @@
 import React from 'react';
 
+const Card = ({ title, descriptions, image, flip }) => {
+  return (
+    <content className={`flex w-full items-center justify-center text-center
+                        flex-col md:flex-row
+                        ${flip ? 'md:flex-row-reverse' : ''}`}>
+      <div className="flex-grow
+                      flex flex-col
+                      items-center text-left justify-center w-full px-10 py-6
+                      ">
+        <h1 className="text-3xl font-bold text-theme-blue-300">
+          <span className="text-theme-blue-300">
+            {title}
+          </span>
+        </h1>
+        {descriptions.map((desc, index) => (
+          <p key={index} className="text-2xl text-theme-blue-300 py-4">
+            {desc}
+          </p>
+        ))}
+      </div>
+
+      <div className="flex-grow
+                      flex
+                      items-center justify-center text-center w-full p-6">
+        <img className="w-11/12" src={image}></img>
+      </div>
+    </content>
+  );
+}
+
 
 const Info = () => {
   return (
     <>
+      <Card
+        title="Randomized email on every site"
+        descriptions={[
+          "Sign up on every site with a random email address, unique to that site.",
+          "Prevent your identity from being correlated across the internet.",
+        ]}
+        image="placeholder.jpg"
+      />
 
-      <content className="flex flex-col w-full items-center justify-center text-center
-                          sm:flex-row">
-        
-          <div className="flex-grow
-                          flex flex-col
-                          items-start text-left justify-center w-full p-6">
-              <h1 className="text-2xl font-bold text-theme-blue-300">
-                <span className="text-theme-blue-300">Randomized email on every site</span>
-              </h1>
-              <p className="text-xl text-theme-blue-300">
-                Sign up on every site with a unique email address to prevent tracking.
-              </p>
-          </div>
+      <Card
+        title="Seamlessly works on any website"
+        descriptions={[
+          "Automatically fills in randomized email addresses on any website for you.",
+          "Our browser extension makes it as easy as clicking a button.",
+        ]}
+        image="placeholder.jpg"
+        flip={true}
+      />
 
-          <div className="flex-grow
-                          flex
-                          items-center justify-center text-center w-full p-6">
-            <img className="h-48 w-80" src="secure-identity-2.jpg"></img>
-          </div>
-      </content>
-      
-
-      <content className="flex flex-col w-full items-center justify-center text-center
-                          sm:flex-row-reverse">
-        
-          <div className="flex-grow
-                          flex flex-col
-                          items-start text-left justify-center w-full p-6">
-              <h1 className="text-2xl font-bold text-theme-blue-300">
-                <span className="text-theme-blue-300">Seamlessly works on any website</span>
-              </h1>
-              <p className="text-xl text-theme-blue-300">
-              Automatically generate a unique email address for every site you visit.
-              </p>
-          </div>
-
-          <div className="flex-grow
-                          flex
-                          items-center justify-center text-center w-full p-6">
-            <img className="h-48 w-80" src="secure-identity-2.jpg"></img>
-          </div>
-      </content>
-
-      <content className="flex flex-col w-full items-center justify-center text-center
-                          sm:flex-row">
-        
-          <div className="flex-grow
-                          flex flex-col
-                          items-start text-left justify-center w-full p-6">
-              <h1 className="text-2xl font-bold text-theme-blue-300">
-                <span className="text-theme-blue-300">Pre-emptive protection against data breaches</span>
-              </h1>
-              <p className="text-xl text-theme-blue-300">
-                Prevent common attacks including 
-                <span className="text-theme-blue-200"> credential stuffing </span>
-                and
-                <span className="text-theme-blue-200"> password spraying.</span> 
-              </p>
-          </div>
-
-          <div className="flex-grow
-                          flex
-                          items-center justify-center text-center w-full p-6">
-            <img className="h-48 w-80" src="secure-identity-2.jpg"></img>
-          </div>
-      </content>
+      <Card
+        title="Pre-emptive protection against data breaches"
+        descriptions={[
+          "Prevent common attacks including credential stuffing and password spraying.",
+          "Easily dispose of compromised email addresses, and identify which sites have been breached.",
+        ]}
+        image="placeholder.jpg"
+      />
 
     </>
   );
