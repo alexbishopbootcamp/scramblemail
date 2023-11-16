3eb03b7bac596b04917ada8a9d7ce4bed4a617fc
Alex <github@alexbi.shop>
2023-11-16 13:36:55 +0900

    Set refresh token in HTTP only cookie

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,7 +12,6 @@ const resolvers = {
         Email.sendVerification({ user });
         return { success: true, message: 'User registered' };
       } catch (err) {
-        console.error(err);
         throw new ApolloError(err.message, 'BAD_USER_INPUT');
       }
     },
@@ -21,7 +20,6 @@ const resolvers = {
         // decode token from base64
         token = Buffer.from(token, 'base64').toString('ascii');
         const data = JWT.verify(token, process.env.JWT_SECRET);
-        console.log(data);
         // Set user to verified
         await User.findByIdAndUpdate(data._id, { verified: true });
         return { success: true, message: 'Email verified' };
@@ -29,7 +27,27 @@ const resolvers = {
         // Throw an ApolloError
         throw new ApolloError('Invalid or expired link', 'TOKEN_INVALID');
       }
-    }
+    },
+    loginUser: async (_, { primaryEmail, password }, context) => {
+      try {
+        const user = await User.findByCredentials(primaryEmail, password);
+        if (!user) {
+          throw new Error('User not found');
+        }
+        const accesstoken = user.generateToken();
+        const refreshtoken = user.generateRefreshToken();
+
+        context.res.cookie('refreshToken', refreshtoken, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'strict'
+        });
+
+        return { success: true, message: 'User logged in', accesstoken, refreshtoken };
+      } catch (err) {
+        throw new ApolloError(err.message, 'BAD_USER_INPUT');
+      }
+    },
   },
 
 };
