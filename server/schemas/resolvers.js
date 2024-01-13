e9e223327ff4bd00034cb76b4ef0963ab5dd6099
Alex <aanderson0@protonmail.com>
2024-01-13 11:03:32 +0900

    Prevent server error messages being displayed to users

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -21,7 +21,11 @@ const resolvers = {
         EmailUtils.sendVerification({ user });
         return { success: true, message: 'A confirmation link has been sent to your email address' };
       } catch (err) {
-        throw new ApolloError(err.message, 'BAD_USER_INPUT');
+        if(err.code == 11000) { // Duplicate key error
+          throw new ApolloError('Email address already in use', 'BAD_USER_INPUT');
+        } else {
+          throw new ApolloError("An error occurred", 'BAD_USER_INPUT');
+        }
       }
     },
     verifyEmail: async (_, { token }, context) => {
@@ -42,11 +46,11 @@ const resolvers = {
       try {
         const user = await User.findByCredentials(primaryEmail, password);
         if (!user) {
-          throw new Error('User not found');
+          throw new ApolloError('User not found');
         }
 
         if (!user.verified) {
-          throw new Error('Please confirm your email address before logging in');
+          throw new ApolloError('Please confirm your email address before logging in');
         }
 
         const accesstoken = signAccessToken(user);
@@ -62,7 +66,7 @@ const resolvers = {
 
         return { success: true, message: 'Logged in successfully', accesstoken };
       } catch (err) {
-        throw new ApolloError(err.message, 'BAD_USER_INPUT');
+        throw new ApolloError("An error occurred", 'BAD_USER_INPUT');
       }
     },
     logoutUser: async (_, args, context) => {
@@ -71,31 +75,32 @@ const resolvers = {
         context.res.clearCookie('refreshToken');
         return { success: true, message: 'Logged out successfully' };
       } catch (err) {
-        throw new ApolloError(err.message, 'BAD_USER_INPUT');
+        console.log(err);
+        throw new ApolloError("An error occurred", 'BAD_USER_INPUT');
       }
     },
     refreshToken: async (_, args, context) => {
       try {
         const token = context.req.cookies.refreshToken;
         if (!token) {
-          throw new Error('No refresh token found');
+          throw new ApolloError('No refresh token found');
         }
 
         const payload = verifyRefreshToken(token);
         const user = await User.findById(payload.data._id);
         if (!user) {
-          throw new Error('User not found');
+          throw new ApolloError('User not found');
         }
 
         const accesstoken = signAccessToken(user);
         return { success: true, message: 'Access token refreshed', accesstoken };
       } catch (err) {
-        throw new ApolloError(err.message, 'BAD_USER_INPUT');
+        throw new ApolloError("An error occurred", 'BAD_USER_INPUT');
       }
     },
     generateAddress: async (_, args, context) => {
       if(!context.req.user) {
-        throw new Error('Not logged in');
+        throw new ApolloError('Not logged in');
       }
 
       // Only attempt generating a unique address a maximum of 5 times
@@ -122,16 +127,16 @@ const resolvers = {
       }
 
       console.error('Error generating address:', err);
-      throw new Error('Failed to generate unique email address'); // Should only be reached if failsafe triggers
+      throw new ApolloError('Failed to generate unique email address'); // Should only be reached if failsafe triggers
     },
     deleteAddress: async (_, { id }, context) => {
       if(!context.req.user) {
-        throw new Error('Not logged in');
+        throw new ApolloError('Not logged in');
       }
       try {
         const email = await Email.findById(id);
         if(!email) {
-          throw new Error('Email not found');
+          throw new ApolloError('Email not found');
         }
         await Email.findByIdAndDelete(id);
         await User.findByIdAndUpdate(context.req.user._id, {
@@ -139,7 +144,7 @@ const resolvers = {
         });
         return { success: true, message: 'Email address deleted' };
       } catch (err) {
-        throw new ApolloError(err.message, 'BAD_USER_INPUT');
+        throw new ApolloError("An error occurred", 'BAD_USER_INPUT');
       }
     },
   },
@@ -150,7 +155,7 @@ const resolvers = {
   Query: {
     getAddresses: async (_, args, context) => {
       if(!context.req.user) {
-        throw new Error('Not logged in');
+        throw new ApolloError('Not logged in');
       }
       try {
         const user = await User.findById(context.req.user._id).populate('emails');
@@ -164,19 +169,19 @@ const resolvers = {
         });
         return emailAddresses;
       } catch (err) {
-        throw new ApolloError(err.message, 'BAD_USER_INPUT');
+        throw new ApolloError("An error occurred", 'BAD_USER_INPUT');
       }
     },
     getProfile: async (_, args, context) => {
       if(!context.req.user) {
-        throw new Error('Not logged in');
+        throw new ApolloError('Not logged in');
       }
 
       try {
         const user = await User.findById(context.req.user._id)
         return { primaryEmail: user.primaryEmail };
       } catch (err) {
-        throw new ApolloError(err.message, 'BAD_USER_INPUT');
+        throw new ApolloError("An error occurred", 'BAD_USER_INPUT');
       }
     }
   }
