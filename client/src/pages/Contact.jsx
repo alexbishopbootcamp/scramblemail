2b76329c44c02494960b30bde2a59d5906aaab9d
Alex <github@alexbi.shop>
2024-01-17 13:47:51 +0900

    Added contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import Splash from '../components/Landing/Splash';
-import Info from '../components/Landing/Info';
 import Header from '../components/common/Header';
 
 const Landing = () => {
   return (
     <div>
       <Header />
-      <Splash />
-      <Info />
+      <div className="flex flex-col m-8 gap-16">
+        <h1 className="font-bold text-theme-blue-300 text-center text-3xl pt-4">Contact</h1>
+        <div className="flex flex-col gap-3 bg-theme-white-200 rounded-xl p-8 shadow-md">
+
+          <p>
+            For assistance with your account, please contact <a className="font-semibold text-theme-blue-200" href="mailto:support@scramble.email">support@scramble.email</a>
+          </p>
+          <p>
+          For any other queries, please contact <a className="font-semibold text-theme-blue-200" href="mailto:contact@scramble.email">contact@scramble.email</a>
+          </p>
+          <p>
+            Please contact us using your registered email address.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
