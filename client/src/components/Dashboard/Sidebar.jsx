bf42e1fcc66457ef4ad42a384818b1d70fefc059
Alex <github@alexbi.shop>
2024-01-17 14:02:47 +0900

    Adjusted dashboard sidebar

--- client/src/components/Dashboard/Sidebar.jsx
+++ client/src/components/Dashboard/Sidebar.jsx
@@ -5,10 +5,10 @@ const Navbar = ({ navItems }) => {
 
 
   return (
-      <div className="flex flex-col gap-2 w-72 bg-azure h-full p-4">
+      <div className="flex flex-col gap-2 w-60 bg-azure h-full p-4">
         {navItems.map((item) => (
           <Link key={item.name} to={item.path}
-            className="block px-2 py-1 text-white text-xl font-bold rounded hover:text-theme-white-600"
+            className="block px-2 py-1 text-white text-xl text-right font-bold hover:text-theme-white-600"
             onClick={() => { item.action?.(); }}>
             {item.name}
           </Link>
